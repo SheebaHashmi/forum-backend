e9b8b4c81ca88deb10938e40f0d50d4bca8e193c
SheebaHashmi <sheebahashmi0@gmail.com>
2022-02-26 17:07:51 -0500

    Create migration and seeds

--- api/data/migrations/20220226204637_first-migration.js
+++ api/data/migrations/20220226204637_first-migration.js
@@ -3,12 +3,26 @@
  * @returns { Promise<void> }
  */
 exports.up = async function(knex) {
-  await knex.schema.createTable('users',(users) => {
+  await knex.schema
+  .createTable('users',(users) => {
       users.increments('user_id')
       users.string('fullname',200).notNullable()
       users.string('username',200).notNullable()
       users.string('password',200).notNullable()
       users.timestamps(false,true)
+  })
+  .createTable('posts',(posts) => {
+      posts.increments('post_id')
+      posts.string('post_title')
+      posts.text('post_body').notNullable()
+      posts.timestamp('created_at').defaultTo(knex.fn.now())
+      posts.integer('author_id')
+        .unsigned()
+        .notNullable()
+        .references('user_id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
 
   })
 };
@@ -18,5 +32,6 @@ exports.up = async function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('posts')
   await knex.schema.dropTableIfExists('users')
 };
---
+++ api/data/seeds/posts.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+      knex('users').del()
+      knex('posts').del()
+
+      // Inserts seed entries
+      await knex('users').insert([
+        {user_id:1,username:"Jane",fullname:"Jane Doe",password:'1234'}
+      ])
+      await knex('posts').insert([
+        {post_id: 1, post_title: 'Feeling the Burn', post_body: 'Stayed in the sun too long, and now I am totally burned!', author_id:'1'},
+        {post_id: 2, post_title: 'Inspiration for today', post_body: 'Happiness is a good flow of life.', author_id:'1'},
+        {post_id: 3, post_title: 'What did Elon Musk said', post_body: 'It would be an incredible adventure. And life needs to be more than just solving every day problems. You need to wake up and be excited about the future', author_id:'1'},
+        
+      ]);
+  
+};
